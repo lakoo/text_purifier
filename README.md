4bf0994dced4e8b0b43f775a1a4b0b5cab7c99c4
Aludirk Wong <aludirk@lakoo.com>
2017-07-20 18:00:46 +0800

    Use Autotools for build system.

--- README.md
+++ README.md
@@ -1,40 +1,23 @@
-# Text Purifier (0.8.3)
+# Text Purifier (0.8.4)
 
 - [Introduction](#introduction)
-- [Compilation](#compilation)
-  - [Static Library](#static-library)
-  - [Shared Library](#shared-library)
+- [Installation](#installation)
 - [License](#license)
 
 ## Introduction
 
 This is a C++ library for purifying text from a list of words.  It supports UTF-8 string (std::wstirng, std::string, wchar_t\*, char\*).
 
-## Compilation
+## Installation
 
-### Static Library
-
-To compile static library (.a):
 ```bash
 git clone https://github.com/lakoo/text_purifier.git
 cd text_purifier
-make static
+./autogen.sh
+./configure
+make
 make install
 ```
-
-The static library is located at `bin/libtextpurifier.a`.
-
-### Shared Library
-
-To compile shared library (.so):
-```bash
-git clone https://github.com/lakoo/text_purifier.git
-cd text_purifier
-make shared
-make install
-```
-
-The shared library is located at `bin/libtextpurifier.so`.
 
 ## License
 
--- doc/html/search/all_6.js
+++ doc/html/search/all_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['text_20purifier_20_280_2e8_2e3_29',['Text Purifier (0.8.3)',['../index.html',1,'']]],
+  ['text_20purifier_20_280_2e8_2e4_29',['Text Purifier (0.8.4)',['../index.html',1,'']]],
   ['text_5fpurifier_2eh',['text_purifier.h',['../text__purifier_8h.html',1,'']]],
   ['textpurifier',['TextPurifier',['../classlakoo_1_1_text_purifier.html',1,'lakoo::TextPurifier'],['../classlakoo_1_1_text_purifier.html#af8f553f1dca1ad70110dd14e872a783d',1,'lakoo::TextPurifier::TextPurifier()'],['../classlakoo_1_1_text_purifier.html#aabaa9cc28934e9ef8a930d6e821efdf3',1,'lakoo::TextPurifier::TextPurifier(const std::list&lt; std::wstring &gt; &amp;list)'],['../classlakoo_1_1_text_purifier.html#aa0788313b1f9bcc6a96a0ccf09d8c839',1,'lakoo::TextPurifier::TextPurifier(const std::list&lt; std::string &gt; &amp;list)'],['../classlakoo_1_1_text_purifier.html#a99488dd2cead65fb7d4f8de98ec41a27',1,'lakoo::TextPurifier::TextPurifier(const wchar_t *const *list, std::size_t count)'],['../classlakoo_1_1_text_purifier.html#ab2e926137c1d58fe13803f6b871c9e55',1,'lakoo::TextPurifier::TextPurifier(const char *const *list, std::size_t count)'],['../classlakoo_1_1_text_purifier.html#add5390a74c2764b2970817ee919bf467',1,'lakoo::TextPurifier::TextPurifier(const TextPurifier &amp;)=delete']]]
 ];
